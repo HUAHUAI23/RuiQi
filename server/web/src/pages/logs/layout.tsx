de3f61c1e5e82d69cb8c7341dc1936f8c44b753b
lim <huahua1319873800@outlook.com>
2025-04-25 05:46:52 +0000

    chore

--- server/web/src/pages/logs/layout.tsx
+++ server/web/src/pages/logs/layout.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/ui/card"
 import { Outlet } from "react-router"
 import { AnimatedRoute } from "@/components/layout/animated-route"
 
-export function LogAndEventPage() {
+export function LogLayOut() {
     return (
         <Card className="flex-1 h-full border-none shadow-none p-0 overflow-hidden">
             <AnimatedRoute>
